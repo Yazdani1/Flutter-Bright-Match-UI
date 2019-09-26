04565832b724265822c212d482e32f86f7fb4b49
Yazdani <yaz4dani@gmail.com>
2019-09-26 23:46:50 +0600

    Second

--- lib/Home.dart
+++ lib/Home.dart
@@ -4,7 +4,6 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => new _HomeState();
 }
-
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
@@ -384,7 +383,7 @@ class _HomeState extends State<Home> {
                   ),
                 ),
 
-                
+
                 //end first container
                 new SizedBox(height: 5.0,),
                 new Container(
